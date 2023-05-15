ae91843734e52a450f3ba3ffe57bfabd43700239
its-mohd-kaif <mohdkaif@cedcoss.com>
2023-05-15 13:08:05 +0530

    FAQ Section Completed

--- src/Components/Panel/Dashboard/FAQ/FAQ.tsx
+++ src/Components/Panel/Dashboard/FAQ/FAQ.tsx
@@ -1,4 +1,4 @@
-import { Accordion, AutoComplete, Button, Card, FlexChild, FlexLayout, PageHeader, Skeleton } from '@cedcommerce/ounce-ui'
+import { Accordion, AutoComplete, Button, Card, FlexChild, FlexLayout, PageHeader, Skeleton, Tag, TextStyles } from '@cedcommerce/ounce-ui'
 import React, { useEffect, useState } from 'react'
 import { DI, DIProps } from "../../../../../src/Core"
 import { urlFetchCalls } from '../../../../../src/Constant'
@@ -6,14 +6,19 @@ import { PlusCircle } from 'react-feather';
 function FAQ(_props: DIProps) {
     const { di: { POST } } = _props;
     const { get: { faqSearch } } = urlFetchCalls;
-    // const [faq, setFaq] = useState<any>([]);
-    const [multiaccor, setMultiacor] = useState<any>([]);
     const [skeleton, setSkeleton] = useState(true);
-    // const [next, setNext] = useState<string | null>(null);
-    // const [allData, setAllData] = useState<any>([]);
-    // const [title, setTitle] = useState<any>([]);
-    const [accordian, setAccordian] = useState<any>([]);
-    const [allData, setData] = useState<any>([])
+    const [data, setData] = useState<any>([]);
+    const [btnLoader, setBtnLoader] = useState<boolean>(false);
+    const [searchLoader, setSearchLoader] = useState<boolean>(false);
+    const [search, setSearch] = useState<string>("");
+    const [options, setOptions] = useState<any>([]);
+    const [searchSelect, setSearchSelect] = useState<any>([]);
+    const [searchAccordian, setSearchAccordian] = useState<boolean>(false);
+
+    /**
+     * in this useEffect we make a post request 
+     * and make data and store into state
+     */
     useEffect(() => {
         setSkeleton(true)
         POST(faqSearch, {
@@ -22,72 +27,109 @@ function FAQ(_props: DIProps) {
         })
             .then((res) => {
                 let tempArr: any = []
-                let tempAccordian: any = []
-                console.log("REs", res.data)
-                let allKeys: any = Object.keys(res.data.meta);
-                console.log("KEYS", allKeys);
-                console.log("CHECK", Object.values(res.data.meta))
                 Object.values(res.data.meta).map((val: any, index: number) => {
                     let dataObj: any = []
-                    console.log("FIRST ", val)
                     val.data.map((item: any) => {
-                        console.log("ITEM", item)
                         let obj = {
                             answer: item.answer,
                             marketplace: item.marketplace,
                             title: item.title,
                             _id: item._id,
-                            open: false
                         }
                         dataObj.push(obj)
                     })
                     let obj = {
                         next_page: val.next_page,
                         group_name: val.group_name,
-                        data: dataObj
+                        data: dataObj,
+                        btn: false
                     }
                     tempArr.push(obj)
                 })
                 setData(tempArr)
                 setSkeleton(false)
-                // setFaq(res.data.meta[Object.keys(res.data.meta)[0]].data)
-                // setNext(res.data.meta[Object.keys(res.data.meta)[0]].next_page)
-                // setAllData(res.data.meta)
-                // setTitle(Object.keys(res.data.meta))
-                setAccordian(tempAccordian)
             })
-
-        let index = 5;
-        while (index !== 1) {
-            setMultiacor([...multiaccor, false])
-            index--
-        }
     }, [])
-
-    const showMoreHandler = () => {
-        // allData.meta[Object.keys(allData.meta)[index + 1]].data.map((val: any) => {
-        //     faq.push(val)
-        //     setFaq([...faq])
-        // })
-        // setNext(allData.meta[Object.keys(allData.meta)[index]].next_page)
-        // setIndex((val) => ++val)
+    /**
+     * show more handler
+     * @param next_page pass next_page string
+     * @param group_name pass group name string
+     */
+    const showMoreHandler = (next_page: any, group_name: any) => {
+        setBtnLoader(true)
+        POST(faqSearch, {
+            "group": group_name,
+            "lastId": next_page,
+            "limit": 5,
+            "marketplace": "meta",
+            "next": true
+        }).then((res) => {
+            setBtnLoader(false)
+            res.data.meta[group_name].data.map((item: any) => {
+                let obj = {
+                    answer: item.answer,
+                    marketplace: item.marketplace,
+                    title: item.title,
+                    _id: item._id,
+                    open: false
+                }
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].group_name.toLowerCase() === group_name) {
+                        data[i].next_page = res.data.meta[group_name].next_page;
+                        data[i].data.push(obj);
+                        setData([...data])
+                    }
+                }
+            })
+        })
     }
-    function capitalizeFirstLetter(string: string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    const accordianHandler = (val: any) => {
-        for (let i = 0; i < accordian.length; i++) {
-            for (let j = 0; j < val.data.length; j++) {
-                if (accordian[i].id === val.data[j]._id) {
-                    console.log("YESS")
-                    accordian[i].open = !accordian[i].open;
-                    setAccordian([...accordian])
+    /**
+     * Accordian open/close handler
+     * @param id pass id for check which accordian hit by user
+     * @returns when condition match then return 
+     */
+    const accordianHandler = (id: any) => {
+        for (let i = 0; i < data.length; i++) {
+            for (let j = 0; j < data[i].data.length; j++) {
+                if (data[i].data[j]._id === id) {
+                    data[i].data[j].open = !data[i].data[j].open;
+                    setData([...data])
                     return
                 }
             }
         }
     }
-    console.log("KAIF", allData)
+    /**
+     * debounce functionality on search handler
+     */
+    useEffect(() => {
+        if (search !== "") {
+            setSearchLoader(true)
+            const getData = setTimeout(() => {
+                POST(faqSearch, {
+                    "keyword": search,
+                    "limit": 5,
+                    "marketplace": "meta"
+                }).then((res) => {
+                    setSearchLoader(false)
+                    let tempArr: any = []
+                    Object.values(res.data.meta).map((val: any) => {
+                        val.data.map((item: any) => {
+                            let obj = {
+                                label: item.title,
+                                value: item.title,
+                                answer: item.answer,
+                                _id: item._id
+                            }
+                            tempArr.push(obj)
+                        })
+                    })
+                    setOptions(tempArr)
+                })
+            }, 3000);
+            return () => clearInterval(getData);
+        }
+    }, [search])
     return (
         <>
             <PageHeader
@@ -95,18 +137,30 @@ function FAQ(_props: DIProps) {
             />
             <FlexLayout direction='vertical' spacing='mediumLoose'>
                 <AutoComplete
-                    clearButton
-                    clearFunction={function noRefCheck() { }}
-                    onChange={function noRefCheck() { }}
-                    onClick={function noRefCheck() { }}
+                    clearButton={true}
+                    clearFunction={() => {
+                        setSearch("")
+                        setSearchSelect([])
+                    }}
+                    onChange={(e: string) => {
+                        setSearch(e)
+                    }}
+                    onClick={(e: any) => {
+                        options.map((val: any) => {
+                            if (val.label === e) {
+                                setSearchSelect(val)
+                            }
+                        })
+                    }}
                     onEnter={function noRefCheck() { }}
-                    options={[]}
+                    options={options}
                     placeHolder="Search Your Items"
                     popoverContainer="body"
                     popoverPosition="right"
                     setHiglighted
                     thickness="thick"
-                    value=""
+                    value={search}
+                    loading={searchLoader}
                 />
                 {skeleton === true ?
                     <FlexLayout spacing='loose' direction='vertical'>
@@ -123,69 +177,61 @@ function FAQ(_props: DIProps) {
                             ))
                         }
                     </FlexLayout> :
-                    allData.map((val: any, index: number) => (
-                        <>
-                            {val.next_page !== null || val.data.length !== 0 ?
-                                <Card title={val.group_name}>
-                                    {val.data.map((item: any, index: number) => (
-                                        <>
-                                            <Accordion
-                                                boxed
-                                                icon
-                                                iconAlign="left"
-                                                onClick={() => accordianHandler(val)}
-                                                title={item.title}
-                                                active={accordian.open}
-                                            >
-                                                <div dangerouslySetInnerHTML={{ __html: item.answer }}></div>
-                                            </Accordion>
-                                        </>
-                                    ))}
-                                </Card> : null
-                            }
-                        </>
-                    ))}
-
-                {/* <Card
-                    title={"Common Queries"}
-                >
-                    {skeleton === true ?
-                        <FlexLayout spacing='loose' direction='vertical'>
-                            {
-                                [1, 2, 3, 4, 5].map((val) => (
-                                    <FlexChild key={val}>
-                                        <Skeleton
-                                            height="45px"
-                                            line={1}
-                                            type="line"
-                                            width="50px"
-                                        />
-                                    </FlexChild>
-                                ))
-                            }
-                        </FlexLayout> : faq.map((val: any, index: number) => (
-                            <>
+                    searchSelect.label !== undefined ?
+                        <FlexLayout spacing='extraLoose' direction='vertical'>
+                            <FlexLayout valign='center' spacing='tight'>
+                                <TextStyles fontweight='extraBold' headingTypes='LG-2.8'>
+                                    Search result for:
+                                </TextStyles>
+                                <Tag destroy={() => {
+                                    setSearch("")
+                                    setSearchSelect([])
+                                }}>
+                                    {searchSelect.label}
+                                </Tag>
+                            </FlexLayout>
+                            <Card>
                                 <Accordion
                                     boxed
                                     icon
                                     iconAlign="left"
-                                    onClick={() => {
-                                        multiaccor[index] = !multiaccor[index]
-                                        setMultiacor([...multiaccor])
-                                    }}
-                                    title={val.title}
-                                    active={multiaccor[index]}
+                                    onClick={() => setSearchAccordian(!searchAccordian)}
+                                    title={searchSelect.label}
+                                    active={searchAccordian}
                                 >
-                                    <div dangerouslySetInnerHTML={{ __html: val.answer }}></div>
+                                    <div dangerouslySetInnerHTML={{ __html: searchSelect.answer }}></div>
                                 </Accordion>
+                            </Card>
+
+                        </FlexLayout> :
+                        data.map((val: any, index: number) => (
+                            <>
+                                {val.next_page !== null || val.data.length !== 0 ?
+                                    <Card title={val.group_name}>
+                                        {val.data.map((item: any, index: number) => (
+                                            <>
+                                                <Accordion
+                                                    boxed
+                                                    icon
+                                                    iconAlign="left"
+                                                    onClick={() => accordianHandler(item._id)}
+                                                    title={item.title}
+                                                    active={item.open}
+                                                >
+                                                    <div dangerouslySetInnerHTML={{ __html: item.answer }}></div>
+                                                </Accordion>
+                                            </>
+                                        ))}
+                                        <br></br>
+                                        {
+                                            val.next_page !== null ? <FlexLayout halign='center'>
+                                                <Button disable={btnLoader} onClick={() => showMoreHandler(val.next_page, val.group_name.toLowerCase(),)} icon={<PlusCircle size={18} />} type='Outlined'>Show more</Button>
+                                            </FlexLayout> : null
+                                        }
+                                    </Card> : null
+                                }
                             </>
                         ))}
-                    <br></br>
-                    {next !== null ? <FlexLayout halign='center'>
-                        <Button onClick={showMoreHandler} icon={<PlusCircle size={18} />} type='Outlined'>Show more</Button>
-                    </FlexLayout> : null}
-
-                </Card> */}
             </FlexLayout>
 
         </>
